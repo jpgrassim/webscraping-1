967f8bb299d710d40f6776aeed58a3d34cdfd484
Juan Pablo Grassi Mazzuchi <juanpablo_grassimaz1@baylor.edu>
2023-04-24 09:37:55 -0500

    jp

--- webscraping-COVID.py
+++ webscraping-COVID.py
@@ -19,8 +19,71 @@ url = 'https://www.worldometers.info/coronavirus/country/us'
 # Request in case 404 Forbidden error
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
+req = Request(url, headers=headers)
 
+webpage = urlopen(req).read()
 
+soup = BeautifulSoup(webpage, 'html.parser')
+
+print(soup.title.text)
+
+table_rows = soup.findAll('tr')
+
+#print(table_rows[2:20])
+
+state_death_ratio = ""
+state_best_testing = ""
+state_worst_testing = ""
+high_death_ratio = 0.0
+high_test_ratio = 0.0
+low_test_ratio = 100.0
+
+for row in table_rows[2:52]:
+    td = row.findAll("td")
+    state = td[1].text.strip('\n')
+    cases = int(td[2].text.replace(",",""))
+    deaths = int(td[4].text.replace(",",""))
+    tested = int(td[10].text.replace(",",""))
+    pop = int(td[12].text.replace(",",""))
+    #print(f"State: {state}")
+    #print(f"Total Cases: {cases}")
+    #print(f"Total Deaths: {deaths}")
+    #print(f"Total Tests: {tested}")
+    #print(f"Total Population: {pop}")
+    
+
+    death_ratio = deaths/cases  
+    test_ratio = tested/pop
+    
+    if death_ratio > high_death_ratio:
+        state_death_ratio = state
+        high_death_ratio = death_ratio
+
+    if test_ratio > high_test_ratio:
+        state_best_testing = state
+        high_test_ratio = test_ratio
+
+    
+    if test_ratio < low_test_ratio:
+        state_worst_testing = state
+        low_test_ratio = test_ratio
+        
+
+    
+        
+
+print(f"State with the highest death ratio is {state_death_ratio}")    
+print('Death ratio:', format(high_death_ratio,'.2%'))
+print()
+print()
+print(f'State with the highest test ratio is {state_best_testing}')
+print('Test ratio: ',format(high_test_ratio, '.2%'))    
+print()
+print()
+print(f"State with the worst testing ratio {state_worst_testing}")
+print('Test ratio: ', format(low_test_ratio, '.2%'))
+#tr -- table row 
+#td -- table cell or column 
 
 
 
